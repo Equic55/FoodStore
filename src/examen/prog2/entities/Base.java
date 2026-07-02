5acb3e33c999384a1cff71d453a8b65c672de4ba
Equic55 <equic55@gmail.com>
2026-07-02 14:31:04 -0300

    Update Base.java

--- src/examen/prog2/entities/Base.java
+++ src/examen/prog2/entities/Base.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -12,9 +13,9 @@ public abstract class Base {
     private static long contador = 1; // contador global para IDs únicos
     private final Long id;            // id único de cada entidad
     private boolean eliminado;        // baja lógica
-    private final LocalDateTime createdAt; // fecha de creacion
+    private final LocalDateTime createdAt; // fecha de creación
 
-    // Constructor  asigna id único a cada objeto
+    // Constructor asigna id único a cada objeto
     public Base() {
         this.id = contador++;
         this.eliminado = false;
